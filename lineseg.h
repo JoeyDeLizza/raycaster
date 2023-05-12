f696244069a3214ff238f4f9359d9e93a555a446
Joey DeLizza <delizza1@go.stockton.edu>
2023-05-11 22:31:49 -0400

    Created a data structure to hold line segments

---
+++ lineseg.h
@@ -0,0 +1,7 @@
+class lineseg {
+ public:
+  
+  int x1, y1, x2, y2;
+  Uint32 color;
+  lineseg(int _x1, int _y1, int _x2, int _y2, Uint32 _color) : x1(_x1), y1(_y1), x2(_x2), y2(_y2), color(_color) {}
+};
