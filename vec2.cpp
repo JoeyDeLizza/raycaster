bab3c5a3289467fa030e0bf8732a51cfb48f7a6b
Joey DeLizza <delizza1@go.stockton.edu>
2023-05-22 20:03:45 -0400

    added vector operations

---
+++ vec2.cpp
@@ -0,0 +1,44 @@
+// vec2
+#include "vec2.h"
+
+
+vec2 vec2::add(vec2 b) {
+  vec2 c = vec2(this->x + b.get_x(), this->y + b.get_y());
+  return c;
+}
+
+vec2 vec2::mul(double m) {
+  return vec2(this->x * m, this->y * m);
+}
+
+double vec2::dot(vec2 b) {
+  return this->x * b.get_x() + this->y * b.get_y();
+}
+
+double vec2::len() {
+  return sqrt(this->x * this->x + this->y * this->y);
+}
+
+void vec2::operator=(vec2 b) {
+  this->x = b.get_x();
+  this->y = b.get_y();
+}
+//vec2 vec2::cross(vec2 b) {
+  //  return this->dot(a) 
+//
+double vec2::get_x() {
+  return this->x;
+}
+
+double vec2::get_y() {
+  return this->y;
+}
+
+//
+vec2 vec2::operator+(vec2 b) {
+  return this->add(b);
+}
+//
+//vec2 vec2::operator-(vec2 b) {
+//  
+//}
---
+++ vec2.h
@@ -0,0 +1,39 @@
+#ifndef VEC2_H
+#define VEC2_H
+
+#include <math.h>
+//2-Dimensional vector
+
+class vec2 {
+ public:
+ vec2(double _x, double _y) : x(_x), y(_y) {}
+
+  vec2() : x(0), y(0) {}
+
+  vec2 add(vec2 b);
+
+  vec2 mul(double m);
+
+  double dot(vec2 b);
+
+  double len();
+
+  double cross();
+
+  double get_x();
+
+  double get_y();
+
+  vec2 operator+(vec2 b);
+
+  vec2 operator-();
+
+  void operator=(vec2 b);
+  
+
+ private:
+  double x, y;
+  double* x_ptr, y_ptr;
+};
+
+#endif
